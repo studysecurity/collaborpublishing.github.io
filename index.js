413174258ae1a52acd3f83a95a004c7bf4b71e26
ssp5746 <ssp@gmail.com>
2020-09-07 00:26:14 +0900

    헤더 서브메뉴 추가 및 카로셀 추가

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const navIcon = document.querySelector(".nav-icon");
-
 navIcon.addEventListener("click", (event) => {
   if (event.target.tagName === "DIV" || event.target.tagName === "SPAN") {
     if (event.target.tagName === "DIV") {
@@ -11,3 +10,45 @@ navIcon.addEventListener("click", (event) => {
     }
   }
 });
+
+const buttons = {
+  prev: document.querySelector(".left-btn"),
+  next: document.querySelector(".right-btn"),
+};
+const cardContainerEl = document.querySelector(".card-container");
+
+// 왼쪽 오른쪽 슬라이드 버튼
+buttons.next.addEventListener("click", () => swapCards("right"));
+buttons.prev.addEventListener("click", () => swapCards("left"));
+
+function swapCards(direction) {
+  swapCardsClass(direction);
+}
+
+function swapCardsClass(direction) {
+  const currentCardEl = cardContainerEl.querySelector(".current-card");
+  const prevCardEl = cardContainerEl.querySelector(".prev-card");
+  const nextCardEl = cardContainerEl.querySelector(".next-card");
+  const prevLeftCardEl = cardContainerEl.querySelector(".prev-left-card");
+  const nextRightCardEl = cardContainerEl.querySelector(".next-right-card");
+
+  currentCardEl.classList.remove("current-card");
+  prevCardEl.classList.remove("prev-card");
+  nextCardEl.classList.remove("next-card");
+  prevLeftCardEl.classList.remove("prev-left-card");
+  nextRightCardEl.classList.remove("next-right-card");
+
+  if (direction === "right") {
+    currentCardEl.classList.add("prev-card");
+    prevCardEl.classList.add("prev-left-card");
+    prevLeftCardEl.classList.add("next-right-card");
+    nextCardEl.classList.add("current-card");
+    nextRightCardEl.classList.add("next-card");
+  } else if (direction === "left") {
+    currentCardEl.classList.add("next-card");
+    prevCardEl.classList.add("current-card");
+    prevLeftCardEl.classList.add("prev-card");
+    nextCardEl.classList.add("next-right-card");
+    nextRightCardEl.classList.add("prev-left-card");
+  }
+}
